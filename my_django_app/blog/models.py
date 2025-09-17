463ffff28e8c1e03a798d46d1ffafeffc3060c64
Shorena Nemsadze <shorenanemsadze96@gmail.com>
2025-09-16 18:38:04 -0700

    Create blog Model

--- my_django_app/blog/models.py
+++ my_django_app/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(verbose_name='სათაური', max_length=100)
+    text = models.TextField(verbose_name='ტექსტი')
+
+    class Meta:
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
+
+    def __str__(self):
+        return self.title
